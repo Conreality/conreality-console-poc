4887f04b749152c046c682fda5f407360159be41
Arto Bendiken <arto@conreality.org>
2017-05-12 15:51:29 +0200

    Ensured that the app exits on errors in the QML files.

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,9 @@ main(int argc, char* argv[]) {
 
   QQmlApplicationEngine engine;
   engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+  if (engine.rootObjects().isEmpty()) {
+    return -1;
+  }
 
   return app.exec();
 }
