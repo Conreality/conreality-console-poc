63781ad505d3b279d3be9a49b000a8d70ca7d2a8
Arto Bendiken <arto@conreality.org>
2017-06-07 23:49:29 +0200

    Changed to using player credentials for the database connection.

--- src/main.cpp
+++ src/main.cpp
@@ -2,27 +2,45 @@
 
 #include <QtGlobal> /* for qUtf8Printable() */
 #include <QApplication>
+#include <QProcessEnvironment>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
 #include <QSqlDatabase>
 #include <QSqlError>
+#include <QSqlQuery>
 
 #include "TableModel.h"
+
+static void
+execute_or_die(const char* const sql) {
+  QSqlQuery sql_query(sql);
+  const auto error = sql_query.lastError();
+  if (error.isValid()) {
+    qFatal("PostgreSQL: %s.", qUtf8Printable(error.text()));
+  }
+}
 
 int
 main(int argc, char* argv[]) {
   QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
   QApplication app(argc, argv);
+  const auto env = QProcessEnvironment::systemEnvironment();
 
   QSqlDatabase db = QSqlDatabase::addDatabase("QPSQL");
   db.setHostName("localhost");
   db.setPort(5432);
-  if (!db.open()) {
+  db.setDatabaseName(env.value("USER", "postgres"));
+  if (!db.open("00000000-0000-0000-0000-000000000000", "")) {
     const auto error = db.lastError();
     qFatal("PostgreSQL: %s.", qUtf8Printable(error.text()));
     return -1;
   }
+
+  execute_or_die("SELECT public.session_start()");
+  app.connect(&app, &QApplication::aboutToQuit, []{
+    execute_or_die("SELECT public.session_terminate()");
+  });
 
   QQmlApplicationEngine engine;
   auto rootContext = engine.rootContext();
