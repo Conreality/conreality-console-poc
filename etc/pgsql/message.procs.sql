79ebbccee67a1b5c8dd6c4a1253680d0a92e0a16
Arto Bendiken <arto@conreality.org>
2017-06-07 15:59:35 +0200

    Renamed send_message() to message_send().

--- etc/pgsql/message.procs.sql
+++ etc/pgsql/message.procs.sql
@@ -1,10 +1,10 @@
-DROP FUNCTION IF EXISTS public.send_message(text, text) RESTRICT;
+DROP FUNCTION IF EXISTS public.message_send(text, text) RESTRICT;
 
-CREATE FUNCTION public.send_message(sender text, text text) RETURNS bigint AS $$
+CREATE FUNCTION public.message_send(message_sender text, message_text text) RETURNS bigint AS $$
   local query  = "INSERT INTO public.message (sender, text) VALUES ($1::uuid, $2) RETURNING id";
   local insert = server.prepare(query, {"text", "text"});
-  local result = insert:execute({sender, text}, false, 1);
-  local id = result[1].id -- an int64
-  server.execute("NOTIFY message, '" .. tostring(id) .. "'", false);
-  return id;
+  local result = insert:execute({message_sender, message_text}, false, 1);
+  local message_id = result[1].id -- an int64
+  server.execute("NOTIFY message, '" .. tostring(message_id) .. "'", false);
+  return message_id;
 $$ LANGUAGE pllua;
