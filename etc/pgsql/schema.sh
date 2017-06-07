3a4f4a69994d5df08fcf0b2740c53afbe209f826
Arto Bendiken <arto@conreality.org>
2017-06-07 23:35:11 +0200

    Added missing files to the overall SQL schema script.

--- etc/pgsql/schema.sh
+++ etc/pgsql/schema.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#cat extensions.sql;    echo
+cat extensions.sql;    echo
 
 # dependency order:
 cat theater.table.sql; echo
@@ -11,9 +11,13 @@ cat object.procs.sql;  echo
 cat asset.table.sql;   echo
 cat camera.table.sql;  echo
 cat event.table.sql;   echo
+cat event.procs.sql;   echo
 cat message.table.sql; echo
 cat message.procs.sql; echo
 cat player.table.sql;  echo
+cat player.procs.sql;  echo
+cat point.procs.sql;   echo
+cat session.procs.sql; echo
 cat target.table.sql;  echo
 
 # dependency order:
