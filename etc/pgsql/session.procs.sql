9042278252d592826e8933650580edb6c3e83c0d
Arto Bendiken <arto@conreality.org>
2017-06-07 23:41:07 +0200

    Ensured that the session procedures do a NOTIFY.

--- etc/pgsql/session.procs.sql
+++ etc/pgsql/session.procs.sql
@@ -5,10 +5,12 @@ CREATE FUNCTION public.session_start() RETURNS void AS $$
   UPDATE public.player
     SET ip_addr = inet_client_addr()
     WHERE uuid = session_user::uuid;
+  SELECT pg_notify('session.start', session_user);
 $$ LANGUAGE sql VOLATILE PARALLEL UNSAFE;
 
 CREATE FUNCTION public.session_terminate() RETURNS void AS $$
   UPDATE public.player
     SET ip_addr = NULL
     WHERE uuid = session_user::uuid;
+  SELECT pg_notify('session.terminate', session_user);
 $$ LANGUAGE sql VOLATILE PARALLEL UNSAFE;
