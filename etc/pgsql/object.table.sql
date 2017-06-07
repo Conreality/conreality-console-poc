bb4a7017f095d8cb63262e96e8ee5c50fe39e5ed
Arto Bendiken <arto@conreality.org>
2017-06-07 22:36:04 +0200

    Made objects' theater information optional.
    Newly-registered players don't necessarily have an assigned theater.

--- etc/pgsql/object.table.sql
+++ etc/pgsql/object.table.sql
@@ -12,12 +12,12 @@ CREATE TYPE public.object_type AS ENUM (
 CREATE TABLE public.object (
   -- The object's unique identifier.
   uuid        uuid NOT NULL PRIMARY KEY,
-  -- The theater that the object is located in.
-  theater     uuid NOT NULL REFERENCES public.theater ON DELETE CASCADE,
-  -- The group, if any, that the object is located in.
-  "group"     uuid NULL REFERENCES public.group ON DELETE SET NULL,
   -- The object's type.
   type        object_type NULL,
+  -- The theater that the object is located in.
+  theater     uuid NULL REFERENCES public.theater ON DELETE SET NULL,
+  -- The group, if any, that the object is located in.
+  "group"     uuid NULL REFERENCES public.group ON DELETE SET NULL,
   -- The object's designated label.
   label       varchar(31) NULL CHECK (label <> ''),
   -- The object's current position (as 3D coordinates relative to its theater).
