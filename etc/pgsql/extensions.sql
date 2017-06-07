814ce7761c335f162bcf1630b35155a5d3adcf5a
Arto Bendiken <arto@conreality.org>
2017-06-07 22:34:35 +0200

    Enabled the uuid-ossp extension.

--- etc/pgsql/extensions.sql
+++ etc/pgsql/extensions.sql
@@ -1,13 +1,13 @@
+-- Enable UUID generation
+-- See: https://www.postgresql.org/docs/current/static/uuid-ossp.html
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 -- Enable PL/Lua
-CREATE EXTENSION pllua;
+-- See: http://pllua.projects.pgfoundry.org/
+CREATE EXTENSION IF NOT EXISTS pllua;
 
 -- Enable PostGIS (includes raster)
-CREATE EXTENSION postgis;
-
--- Enable Topology
-CREATE EXTENSION postgis_topology;
-
--- Enable PostGIS Advanced 3D
--- and other geoprocessing algorithms
--- sfcgal not available with all distributions
--- CREATE EXTENSION postgis_sfcgal;
+-- See: http://postgis.net/
+CREATE EXTENSION IF NOT EXISTS postgis;
+CREATE EXTENSION IF NOT EXISTS postgis_topology;
+-- CREATE EXTENSION IF NOT EXISTS postgis_sfcgal;
